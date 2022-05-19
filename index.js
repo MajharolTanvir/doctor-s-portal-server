eeee76fa6892f5d6e3baf6605d9fcafef40eca59
Majharul tanvir <mdtanvir6070@gmail.com>
2022-05-19 19:56:57 +0600

    admin done

--- index.js
+++ index.js
@@ -106,6 +106,14 @@ async function run() {
             res.send({ result, token });
         })
 
+        // //////////////////admin/////////////////////////
+        app.get('/admin/:email', async (req, res) => {
+            const email = req.params.email;
+            const user = await userCollection.findOne({ email: email })
+            const isAdmin = user.role === 'admin'
+            res.send({ admin: isAdmin })
+        })
+
         // /////////////////////Get Admin/////////////////////
         app.put('/user/admin/:email', verifyJWT, async (req, res) => {
             const email = req.params.email;
